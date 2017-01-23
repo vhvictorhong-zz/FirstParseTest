d49032fc3188a259b899a921207e9cdd0c590e6f
victorhong1988 <victorhong1988@gmail.com>
2017-01-23 14:19:34 -0500

    Retrieve data from parse

---
+++ myFirstParseServer/TableViewController.swift
@@ -0,0 +1,67 @@
+//
+//  TableViewController.swift
+//  myFirstParseServer
+//
+//  Created by Victor Hong on 23/01/2017.
+//  Copyright © 2017 Victor Hong. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class TableViewController: UITableViewController {
+
+    var foodArray: [PFObject]?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let pfQuery = PFQuery(className: "Food")
+        
+        pfQuery.findObjectsInBackground { (foodItems, error) in
+            
+            self.foodArray = foodItems
+            self.tableView.reloadData()
+        }
+        
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        if let foodArray = foodArray {
+            return foodArray.count
+        }
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+
+        // Configure the cell...
+        if let foodArray = foodArray {
+            cell.textLabel?.text = foodArray[indexPath.row].object(forKey: "name") as! String?
+        } else {
+            cell.textLabel?.text = "\(indexPath.row)"
+        }
+        
+        return cell
+    }
+ 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
